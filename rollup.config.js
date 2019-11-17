c12413603d71eef5e4a5b38ac787489c63349ab2
hontas <lundin.pontus@gmail.com>
2019-11-17 01:15:38 +0100

    glob it

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
-import fs from 'fs';
-import path from 'path';
-
+import glob from 'glob';
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
@@ -11,11 +9,7 @@ import postcss from 'rollup-plugin-postcss';
 const svelteConfig = require('./svelte.config');
 
 const production = !process.env.ROLLUP_WATCH;
-const svelteDir = 'src/svelte';
-const svelteFiles = fs
-  .readdirSync(path.resolve(__dirname, svelteDir))
-  .filter((file) => file.endsWith('.svelte'))
-  .map((file) => `${svelteDir}/${file}`);
+const svelteFiles = glob.sync('src/svelte/**/*.svelte');
 
 export default {
   input: ['src/index.js', 'src/litElement/toggle-lit.js', 'src/custom-elements/toggle-native.js', ...svelteFiles],
@@ -31,7 +25,6 @@ export default {
     }),
     svelte({
       dev: !production,
-      // Tell the compiler to output a custom element.
       ...svelteConfig
     }),
     resolve(),
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  // Tell the compiler to output a custom element.
   customElement: true
 };
